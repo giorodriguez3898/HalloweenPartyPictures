133355b00cbc1332865605f203950e0429b72d4b
Sergio Rodriguez <giorodriguez3898@gmail.com>
2021-10-29 14:19:47 -0700

    stuff

--- src/app/views/stage/stage.component.ts
+++ src/app/views/stage/stage.component.ts
@@ -15,8 +15,9 @@ export class StageComponent implements OnInit {
   imageName: string = "eating.jpg";
   base64Data: any;
   retrievedImage: any = null;
-  currentSlide = 0;
+  public currentImage = 0;
   public database_populated: boolean = false;
+  public nameList: any;
   totalImages: any;
 
   ngOnInit(): void {
@@ -51,28 +52,54 @@ export class StageComponent implements OnInit {
     { name: "P.Diddy", src: "https://media.istockphoto.com/photos/mount-fitz-roy-with-laguna-de-los-tres-and-laguna-sucia-patagonia-picture-id1308246314?b=1&k=20&m=1308246314&s=170667a&w=0&h=6ZtwcQfB4y1Ik3fce71pTNYsFav0eSXOZuFh8jhdm2k=" }
   ];
 
+  sleep(ms) {
+       return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
 
   cycle(){
 
-    interval(30000).subscribe(x => { 
+    interval(20000).subscribe(x => { 
+
       this.updateCount();
+      console.log("executing update count")
+      this.sleep(10000)
+      console.log("executing update names list")
       this.updateNamesList()
+      this.sleep(10000)
+      if (this.currentImage == this.totalImages){
+        this.currentImage = this.currentImage % this.totalImages;
+        console.log("mod used")
+        console.log(this.currentImage)
+      }
+      else
+      {
+        console.log("mod not used")
+        this.currentImage = this.currentImage + 1;
+        console.log("check 1")
+        this.imageName = this.nameList[this.currentImage - 1]
+        console.log(this.nameList)
+        console.log("check 2")
+        this.getImage()
+      }
       //this.getImage();
     });
   }
 
   getImage() {
     //Make a call to Sprinf Boot to get the Image Bytes.
+    console.log("getting image")
     this.httpClient.get('http://localhost:8080/halloween/get/' + this.imageName)
       .subscribe(
         res => {
+          console.log("in subscribe")
           this.retrieveResponse = res;
           this.base64Data = this.retrieveResponse.picByte;
           this.retrievedImage = 'data:image/jpeg;base64,' + this.base64Data;
         }
       );
 
-      this.slides.push({name: "tester", src: this.retrievedImage})
+      //this.slides.push({name: "tester", src: this.retrievedImage})
   }
 
   updateNamesList(){
@@ -80,6 +107,7 @@ export class StageComponent implements OnInit {
       .subscribe(
         res => {
           console.log("printing update names list")
+          this.nameList = res;
           console.log(res)
         }
       );
@@ -87,6 +115,7 @@ export class StageComponent implements OnInit {
 
 
   updateCount(){
+    console.log("inside update count")
      this.httpClient.get('http://localhost:8080/halloween/check')
       .subscribe(
         res => {
